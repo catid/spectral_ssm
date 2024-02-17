bf1fd18b78a79b22f3f9e70453dc92ee3a9e73a2
catid <mrcatid@gmail.com>
2024-02-17 21:17:57 +0000

    wip

--- ar_stu.py
+++ ar_stu.py
@@ -7,9 +7,9 @@ import math
 from convolutions import ConvolutionLayer
 from autoregressive import AutoRegressiveCausalInput, AutoRegressiveCausalOutput
 
-class AR_STULayer1(nn.Module):
+class AR_STULayer(nn.Module):
     def __init__(self, D_in, D_out, L, K=16, Ku=3, Ky=2, alpha=0.9):
-        super(AR_STULayer1, self).__init__()
+        super(AR_STULayer, self).__init__()
 
         # Autoregressive sum for u (input) from Eq. 6
         self.autoregressive_u = AutoRegressiveCausalInput(D_in, D_out, Ku=Ku)
@@ -34,10 +34,3 @@ class AR_STULayer1(nn.Module):
 
         y = y.permute(0, 2, 1) # Convert to [B, L, D]
         return y
-
-class AR_STULayer(nn.Module):
-    def __init__(self, D_in, D_out, L, K=16, Ku=3, Ky=2, alpha=0.9):
-        super(AR_STULayer, self).__init__()
-
-    def forward(self, u):
-        return u
